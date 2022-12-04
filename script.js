e3a1169814145e37a109148a8abd1fb8f4014ac1
Eric Winebrenner <ejwinebrenner@gmail.com>
2022-12-03 19:19:28 -0500

    v2 student repo project

--- script.js
+++ script.js
@@ -15,3 +15,35 @@ function randomNumber(x, generatedNumber) {
 
 
 
+//* Have the Computer Guess your Number ---------------------------------------------
+/* 
+    Build out the functionality for the computer to guess and evaluate your random number.
+
+    - There are two static functions framed. DO NOT alter the name or parameters of these functions:
+        - startCompGuess(num)
+        - compGuess(reply)
+
+    *hint:
+        - don't forget that variables are a way for us to store information and update later.
+        - What is the "current number"? The lowest number? Highest?
+
+    You are not limited to just these functions. Feel free to create a new function that may be called to help manage the flow of your code.
+*/
+
+function startCompGuess(num) {
+    // This should return a string that denotes the first guessed number
+
+    // YOUR CODE ...
+
+
+}
+
+function compGuess(reply) {
+    /* 
+    *   The parameter "reply" will either be passing "lower", "correct", or "higher". This should be considered when evaluating th elogic and response.
+
+    This should return a string indicating the computers response.
+    */
+
+}
+
